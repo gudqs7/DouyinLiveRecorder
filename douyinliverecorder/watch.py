6071736f5e232873df87dd89609581a384cab887
gudqs7 <guddqs@icloud.com>
2024-12-17 16:23:48 +0800

    feat(wq): 优化

--- douyinliverecorder/watch.py
+++ douyinliverecorder/watch.py
@@ -15,11 +15,11 @@ from PIL import Image
 from pyautogui import locate
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
+from douyinliverecorder.utils import logger
 
 from msg_push import xizhi
 from wq import config_list
 from wq import xizhi_api_url
-
 
 script_path = os.path.split(os.path.realpath(sys.argv[0]))[0]
 temp_dir_path = f'{script_path}/downloads/temp'
@@ -29,8 +29,8 @@ if os.path.exists(temp_dir_path):
 if not os.path.exists(temp_dir_path):
     os.makedirs(temp_dir_path)
 
-# 创建一个包含5个线程的线程池
-file_handle_executor = ThreadPoolExecutor(max_workers=20, thread_name_prefix='file_handle_executor')
+# 文件创建时间
+file_status = {}
 
 
 def locate_0(search_img: str, big_img: str, region, confidence, print_error: bool = False):
@@ -88,7 +88,7 @@ def send_result_msg(author_name, img_path, title):
         img_data = quote('data:image/png;base64,' + img_base64)
         xizhi(xizhi_api_url, author_name + title, '成绩截图：  \n![成绩截图](' + img_data + ')')
     except Exception as e:
-        print(f'发送微信消息时报错: {e}')
+        logger.error(f'发送微信消息时报错: {e}')
 
 
 def check(search_img, new_path, print_error: bool = False):
@@ -107,17 +107,7 @@ def check(search_img, new_path, print_error: bool = False):
     return has_any_right
 
 
-def check_file(file_path, author_name):
-    # 先休息一会,等待图片完全输出
-    time.sleep(0.5)
-
-    # 移动文件并重命名，目录彻底英文化
-    old_path = file_path
-    old_name = os.path.basename(old_path)
-    new_name = str(time.time()) + '-' + str(uuid.uuid4()) + '.png'
-    new_path = os.path.join(temp_dir_path, new_name)
-    os.rename(old_path, new_path)
-
+def check_file(new_path, author_name, old_name):
     for config in config_list:
         out = config["out"]
         wait_time_sec = config["wait_time_sec"]
@@ -155,12 +145,10 @@ def check_file(file_path, author_name):
                 os.makedirs(out_path)
 
             destination_path = os.path.join(out_path, old_name)
-            time.sleep(0.1)
             # 拷贝单个文件
             shutil.copy2(new_path, destination_path)
 
     # 最后删除图片
-    time.sleep(0.5)
     os.remove(new_path)
 
 
@@ -189,12 +177,31 @@ class Handler(FileSystemEventHandler):
         self.anchor_name = anchor_name
 
     def on_any_event(self, event):
+        png_path = event.src_path
         if event.is_directory:
             return None
         elif event.event_type == 'created':
             try:
                 # 提交任务到线程池
-                file_handle_executor.submit(check_file, event.src_path, self.anchor_name)
+                file_status[png_path] = time.time()
             except Exception as e:
-                print(f'监控文件创建-挨个处理时报错: {e}')
-                # logger.error(f'监控文件创建-挨个处理时报错: {e}')
+                logger.error(f'监控文件创建-挨个处理时报错: {e}')
+        elif event.event_type == 'modified':
+            try:
+                create_time = file_status.get(png_path, 0)
+                if create_time > 0:
+                    del file_status[png_path]
+                    # 移动文件并重命名，目录彻底英文化
+                    old_name = os.path.basename(png_path)
+                    new_name = str(time.time()) + '-' + str(uuid.uuid4()) + '.png'
+                    new_path = os.path.join(temp_dir_path, new_name)
+                    while True:
+                        try:
+                            time.sleep(0.2)
+                            os.rename(png_path, new_path)
+                            break
+                        except Exception as e:
+                            logger.error(f'重命名失败: {e}')
+                    check_file(new_path, self.anchor_name, old_name)
+            except Exception as e:
+                logger.error(f'监控文件修改-挨个处理时报错: {e}')
